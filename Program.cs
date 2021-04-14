49795cd9648171e04482cc13ddde71bab91c94c4
Danny Le <le.dannyj@gmail.com>
2021-04-14 17:33:25 -0400

    Finished deck working on shuffle

--- Program.cs
+++ Program.cs
@@ -9,23 +9,51 @@ namespace AllCardsOnDeckCS
         {
             Console.WriteLine("------- All Cards On Deck -------");
 
-            var cards = new List<string>() { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
-
+            var cards = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
             var suits = new List<string>() { "Spades", "Clubs", "Diamonds", "Hearts" };
 
-            var cardList = new List<String>();
+            var deckList = new List<String>();
 
-            for (var index = 0; index < cards.Count; index++)
+            for (var d = 0; d < cards.Count; d++)
             {
-                for (var cardIndex = 0; cardIndex < suits.Count; cardIndex++)
+                for (var b = 0; b < suits.Count; b++)
                 {
-                    var allCards = $"{} of {}"
-                    cardList.Add()
+                    var allCards = ($"{cards[d]} of {suits[b]}");
+                    deckList.Add(allCards);
                 }
 
+            }
 
+            foreach (var all in deckList)
+
+            {
+                Console.WriteLine($"{all}");
+            }
+
+
+            Console.WriteLine();
+
+
+            var numberOfCards = cards.Count;
+
+            for (var rightIndex = numberOfCards - 1; rightIndex >= 0; rightIndex--)
+            {
+                var leftIndex = new Random().Next(0, rightIndex);
+
+                var rightCard = cards[leftIndex];
+
+                cards[rightIndex] = cards[leftIndex];
+
+                cards[leftIndex] = rightCard;
 
             }
+
+            foreach (var all in cards)
+
+            {
+                Console.WriteLine($"{all}");
+            }
+
 
         }
     }
